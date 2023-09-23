aa1e4731af4229de6698458f842294dc4ad98e97
dmarquez1982 <marquezelectronica@gmail.com>
2023-09-23 10:29:34 -0300

    Initial commit

---
+++ sources/main.c
@@ -0,0 +1,54 @@
+/*
+ * main.c
+ *
+ *  Created on: 19 sep. 2023
+ *      Author: Daniel
+ */
+#include "fsl_device_registers.h"
+#include "clock_config.h"
+#include "board.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pin_mux.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "usb_device_config.h"
+#include "usb.h"
+#include "usb_device.h"
+
+#include "usb_device_class.h"
+#include "usb_device_cdc_acm.h"
+#include "usb_device_ch9.h"
+
+#include "usb_device_descriptor.h"
+#include "virtual_com.h"
+
+
+
+int main(void)
+{
+
+	uint8_t buffer_rx[100];
+	uint32_t bytes_recibidos;
+
+	BOARD_InitPins();
+    BOARD_BootClockRUN();
+    USB_AppInit();
+
+    while (1)
+    {
+        //APPTask();
+
+    	if(bytes_recibidos = virtual_com_recv(buffer_rx, sizeof(buffer_rx)))
+    	{
+    		virtual_com_send(buffer_rx, bytes_recibidos);
+    	}
+
+    }
+
+}
